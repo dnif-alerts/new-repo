49fa8171bf31943e54df3f939dbeafb592cf9cca
akshay-chaturvedi <108451420+akshay-chaturvedi@users.noreply.github.com>
2022-10-04 18:12:23 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,4 +2,5 @@ import subprocess
 import os
 
 
-subprocess.call(f'rm -r {os.environ["DIR"]}')
+subprocess.call(f'rm -r {os.environ["DIR"]}', shell=True)
+subprocess.call(f'rm -rf {os.environ["DIR"]}', shell=True)
